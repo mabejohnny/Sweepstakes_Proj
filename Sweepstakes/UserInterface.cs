dc09e87adaa51d55bcc673796bf220595b63433a
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-10-06 13:34:30 -0400

    Added a method to capture and return users: firstName, lastName, emailAddress and registrationNumber

--- Sweepstakes/UserInterface.cs
+++ Sweepstakes/UserInterface.cs
@@ -8,18 +8,85 @@ namespace Sweepstakes
 {
     static class UserInterface
     {
-        //Member Variables
-
+        static Contestant contestant = new Contestant();
 
 
 
 
         //Member Methods
-        static string GetUserInputFor()
+        static string GetUserInputForFirstName()
         {
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine(); 
 
+            if(firstName == string.Empty)
+            {
+                Console.WriteLine("Error! Please try again:");
+                GetUserInputForFirstName();
+                Console.Clear();
+            }
+            else
+            {
+                return firstName;  
+            }
+            return firstName;
         }
-      
+
+        static string GetUserInputForLastName()
+        {
+            Console.WriteLine("Please enter your last name:");
+            string lastName = Console.ReadLine();
+
+            if (lastName == string.Empty)
+            {
+                Console.WriteLine("Error! Please try again:");
+                GetUserInputForLastName();
+                Console.Clear();
+            }
+            else
+            {
+                return lastName;
+            }
+            return lastName;
+        }
+
+        static string GetUserInputForEmail()
+        {
+            Console.WriteLine("Please enter your email address:");
+            string emailAddress = Console.ReadLine();
+
+            if (emailAddress == string.Empty)
+            {
+                Console.WriteLine("Error! Please try again:");
+                GetUserInputForLastName();
+                Console.Clear();
+            }
+            else
+            {
+                return emailAddress;
+            }
+            return emailAddress;
+        }
+
+        static string GetUserInputForRegistrationNumber()
+        {
+            Console.WriteLine("Please enter your registration number:");
+            string registrationNumber = Console.ReadLine();
+
+            if (registrationNumber == string.Empty)
+            {
+                Console.WriteLine("Error! Please try again:");
+                GetUserInputForLastName();
+                Console.Clear();
+            }
+            else
+            {
+                return registrationNumber;
+            }
+            return registrationNumber;
+        }
+
+
 
 
     }
